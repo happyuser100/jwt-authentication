1c9935591597e36a159652c5c3e6d4f2e76fa4fb
happyuser100 <fleidmos@gmail.com>
2024-10-05 14:56:58 +0300

    JWT authentication that uses Node Js typescript api

---
+++ README.md
@@ -0,0 +1,1 @@
+# jwt-authentication that uses Node Js typescript api
---
+++ src/app/_models/user.ts
@@ -0,0 +1,9 @@
+﻿export class User {
+    id?: number;
+    name?:string;
+    email?: string;
+    username?: string;
+    firstName?: string;
+    lastName?: string;
+    token?: string;
+}
